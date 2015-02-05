433334e999c7fc727d5995a04e9b5b83869913e3
Matt DeWolfe <matthewdewolfe@gmail.com>
2015-02-04 20:48:59 -0500

    Updated with reroute to wix prof

--- js/script.js
+++ js/script.js
@@ -1,6 +1,50 @@
-/**
- * Created by Matts on 05/01/2015.
- */
-$(document).ready(function() {
-    $('#menu').accordion();
-});
+// Created by Matt DeWolfe
+
+var main = function() {
+    $('.dropdown-toggle').click(function() {
+        $('.dropdown-menu').toggle();
+    });
+
+
+    $('.arrow-next').click(function() {
+        var currentSlide = $('.active-slide');
+        var nextSlide = currentSlide.next();
+
+        var currentDot = $('.active-dot');
+        var nextDot = currentDot.next();
+
+        if(nextSlide.length === 0) {
+            nextSlide = $('.slide').first();
+            nextDot = $('.dot').first();
+        }
+
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        nextSlide.fadeIn(600).addClass('active-slide');
+
+        currentDot.removeClass('active-dot');
+        nextDot.addClass('active-dot');
+    });
+
+
+    $('.arrow-prev').click(function() {
+        var currentSlide = $('.active-slide');
+        var prevSlide = currentSlide.prev();
+
+        var currentDot = $('.active-dot');
+        var prevDot = currentDot.prev();
+
+        if(prevSlide.length === 0) {
+            prevSlide = $('.slide').last();
+            prevDot = $('.dot').last();
+        }
+
+        currentSlide.fadeOut(600).removeClass('active-slide');
+        prevSlide.fadeIn(600).addClass('active-slide');
+
+        currentDot.removeClass('active-dot');
+        prevDot.addClass('active-dot');
+    });
+
+}
+
+$(document).ready(main);
